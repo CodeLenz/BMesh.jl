5fca281fb3d829d55af60f4e562b4e90c118f81b
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-16 15:20:52 -0400

    Splitting into 2D and 3D

--- src/bmesh.jl
+++ src/bmesh.jl
@@ -1,13 +1,13 @@
-#
-# basic structure
-#
-struct Bmesh
- 
-    # Dimension (2/3)
-    dimension::Int64
+# Supertype
+abstract type Bmesh end
 
+#
+# basic structure 2D
+#
+struct Bmesh2D <: Bmesh
+ 
     # Element type
-    # :truss2D, :truss3D,...
+    # :truss2D, :solid2D
     etype::Symbol
 
     # Number of nodes
@@ -31,25 +31,68 @@ struct Bmesh
     nz::Int64
  
     # Default constructor
-    function Bmesh(dimension::Int64,etype::Symbol,nn::Int64,ne::Int64,
+    function Bmesh2D(etype::Symbol,nn::Int64,ne::Int64,
                    coord::Matrix{Float64},connect::Matrix{Int64},
                    Lx::Float64, Ly::Float64, Lz::Float64,nx::Int64, ny::Int64, nz::Int64)
 
-        # Basic assertions
-        @assert (dimension==2 || dimension==3) "Bmesh::dimension must be 2 or 3"
-
-        @assert size(coord,1)==nn "Bmesh:: number of rows in coord must be equal to the number of nodes"
-        @assert size(coord,2)==dimension "Bmesh:: number of columns in coord must be equal to dimension"
-        @assert size(connect,1)==ne "Bmesh:: number of rows in connect must be equal to the number of elements"
-        #@assert (etype==:truss2D || etype==:truss3D) && size(connect,2)==2  "Bmesh:: number of columns in connect must be equal to 2 for truss elements"
-        #@assert (etype==:solid2D) && size(connect,2)==4 "Bmesh:: number of columns in connect must be equal to 4 for solid2D elements $etype"
+        @assert size(coord,1)==nn "Bmesh2D:: number of rows in coord must be equal to the number of nodes"
+        @assert size(coord,2)==2 "Bmesh2D:: number of columns in coord must be equal to 2"
+        @assert size(connect,1)==ne "Bmesh2D:: number of rows in connect must be equal to the number of elements"
               
   
         # This should be relaxed for different elements
-        @assert etype==:truss2D || etype==:truss3D || etype==:solid2D  "Bmesh:: just truss (2D and 3D) and solid 2D by now"
+        @assert etype==:truss2D || etype==:solid2D  "Bmesh2D:: just :truss2D and :solid2D by now"
         
         # Creates the data type
-        new(dimension,etype,nn,ne,coord,connect, Lx, Ly, Lz, nx, ny, nz)
+        new(etype,nn,ne,coord,connect, Lx, Ly, Lz, nx, ny, nz)
+    end
+
+end
+
+#
+# basic structure 3D
+#
+struct Bmesh3D <: Bmesh
+ 
+    # Element type
+    # :truss3D, :solid3D
+    etype::Symbol
+
+    # Number of nodes
+    nn::Int64
+    
+    # Number of elements
+    ne::Int64
+    
+    # Nodal coordinates
+    coord::Matrix{Float64}
+    
+    # Element connectivities
+    connect::Matrix{Int64}
+
+    # Dimensions
+    Lx::Float64
+    Ly::Float64
+    Lz::Float64
+    nx::Int64
+    ny::Int64
+    nz::Int64
+ 
+    # Default constructor
+    function Bmesh3D(etype::Symbol,nn::Int64,ne::Int64,
+                   coord::Matrix{Float64},connect::Matrix{Int64},
+                   Lx::Float64, Ly::Float64, Lz::Float64,nx::Int64, ny::Int64, nz::Int64)
+
+        @assert size(coord,1)==nn "Bmesh3D:: number of rows in coord must be equal to the number of nodes"
+        @assert size(coord,2)==3 "Bmesh3D:: number of columns in coord must be equal to 3"
+        @assert size(connect,1)==ne "Bmesh3D:: number of rows in connect must be equal to the number of elements"
+              
+  
+        # This should be relaxed for different elements
+        @assert etype==:truss3D  "Bmesh3D:: just :truss3D  by now"
+        
+        # Creates the data type
+        new(etype,nn,ne,coord,connect, Lx, Ly, Lz, nx, ny, nz)
     end
 
 end
