087532cb528de54ca3ce3fe5e2317d213340abe1
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-16 15:20:17 -0400

    Fixing bad commit

--- src/base.jl
+++ src/base.jl
@@ -1,238 +1,44 @@
-
 #
-# 2D
+# Retorna os nós de um elemento
 #
-function Plot_structure(bmesh::Bmesh2D;
-                        U=[],
-                        dscale = 0.0,
-                        x=[],
-                        N = [],
-                        cutout = 1E-3,
-                        name=""
-                        )
-   
-  
-    # Local aliases
-    ne = bmesh.ne
- 
-    # Create the plot
-    plot();
-  
-    # Testa por x. Se o usuário não informou, usamos unitário
-    if length(x)==0
-        x = ones(ne)
-    end
+function Conect(bmesh::Bmesh,ele::Int64)
 
-    println("Generating the visualization...please wait")
+    # Consistência do elemento
+    0 < ele <= bmesh.ne || throw("Conec::elemento $ele não é válido")
 
-    # Se o vetor N for vazio, enchemos com 1.0
-    if isempty(N)
-         N = ones(ne)
-    end
-    
-
-    # Loop over the elements, extracting the nodes
-    # and their coordinates
-    
-    for ele=1:ne
-
-        # recover the nodes
-        nodes = Conect(bmesh,ele)
-        
-        # recover the coordinates
-        c1 = Coord(bmesh,nodes[1])
-        c2 = Coord(bmesh,nodes[2])
-
-        # Tolerância para comparação
-        tol = 1.0/1E6 
-
-        # Se N[ele] <0.0 -> azul
-        # Se N[ele] >0.0 -> vermelho
-        # Se N[ele] = 0.0 -> verde
-        cor = :blue
-        if N[ele]>0.0 
-            cor = :red
-        end    
-
-        # Se for numericamente próximo de zero
-        if isapprox(abs(N[ele]),0.0,atol=1E-3)
-            cor = :green
-        end
-
-        # Adds a line to the plot
-        # só mostra a linha se o x[ele] > do que o valor de corte
-        if x[ele]>cutout+tol 
-               plot!([c1[1],c2[1]],[c1[2],c2[2]],
-                      color=cor,linewidth=(0.1+5*x[ele]),legend=false)
-         end
-
-    end #ele
-
-    # If the displacement vector is given
-    if length(U)>0
-
-        # Normalize
-        U ./= norm(U)
-
-        # Loop over the elements. Now, the coordinates of each
-        # node are given by the initial coordinates
-        # plus the displacement*dscale
-        for ele=1:ne
-
-            # recover the nodes
-            nodes = Conect(bmesh,ele)
-            
-            # recover the coordinates
-            c1 = Coord(bmesh,nodes[1])
-            c2 = Coord(bmesh,nodes[2])
-
-            # Add the displacements
-            pos = 2*(node1-1)+1; c1[1] +=  U[pos]*dscale
-            pos = 2*(node1-1)+2; c1[2] +=  U[pos]*dscale
-            pos = 2*(node2-1)+1; c2[1] +=  U[pos]*dscale
-            pos = 2*(node2-1)+2; c2[2] +=  U[pos]*dscale
-
-            
-            # Tolerância para comparação
-            tol = 1.0/1E6 
-
-            # Adds a line to the plot
-            if x[ele] >cutout + tol
-                plot!([c1[1],c2[1]],[c1[2],c2[2]],
-                    color=:red,linewidth=(0.1+5*x[ele]),legend=false)
-            end
-
-        end #ele
-
-    end #if length
-
-    # Display the plot
-    display(plot!())
-
-    # If name is not empty, saves the image
-    if !isempty(name)
-        savefig(name*".png")
-    end
+    # Retorna os nós do elemento
+    return bmesh.connect[ele,:]
 
 end
 
-
-
 #
-# 3D
+# Retorna as coordenadas de um nó
 #
-function Plot_structure(bmesh::Bmesh3D;
-                        U=[],
-                        dscale = 0.0,
-                        x=[],
-                        N = [],
-                        cutout = 1E-3,
-                        name=""
-                        )
+function Coord(bmesh::Bmesh2D,node::Int64)
 
+   # Consistência do nó
+   0 < node <= bmesh.nn || throw("Coord::nó $node não é válido")
 
-    # Local aliases
-    ne = bmesh.ne
- 
-    # Starts the plot
-    plot3d();
-
-    # Testa por x. Se o usuário não informou, usamos unitário
-    if length(x)==0
-        x = ones(ne)
-    end
-
-    println("Generating the visualization...please wait")
-
-    # Se o vetor N for vazio, enchemos com 1.0
-    if isempty(N)
-        N = ones(ne)
-    end
-
-
-    # Loop over the elements, extracting the nodes
-    # and their coordinates
-
-    for ele=1:ne
-
-        # recover the nodes
-        nodes = Conect(bmesh,ele)
-                
-        # recover the coordinates
-        c1 = Coord(bmesh,nodes[1])
-        c2 = Coord(bmesh,nodes[2])
-
-        # Tolerância para comparação
-        tol = 1.0/1E6 
-
-        # Se N[ele] <0.0 -> azul
-        # Se N[ele] >0.0 -> vermelho
-        # Se N[ele] = 0.0 -> verde
-        cor = :blue
-        if N[ele]>0.0 
-            cor = :red
-        end    
-
-        # Se for numericamente próximo de zero
-        if isapprox(abs(N[ele]),0.0,atol=1E-3)
-            cor = :green
-        end
-
-        # Adds a line to the plot
-        # só mostra a linha se o x[ele] > do que o valor de corte
-        if x[ele]>cutout+tol 
-            plot3d!([c1[1],c2[1]],[c1[2],c2[2]],[c1[3],c2[3]],
-                    color=cor,linewidth=(0.1+5*x[ele]),legend=false)
-        end
-
-    end #ele
-
-    # If the displacement vector is given
-    if length(U)>0
-
-        # Normalize
-        U ./= norm(U)
-
-        # Loop over the elements. Now, the coordinates of each
-        # node are given by the initial coordinates
-        # plus the displacement*dscale
-        for ele=1:ne
-
-            # recover the nodes
-            nodes = Conect(bmesh,ele)
-                    
-            # recover the coordinates
-            c1 = Coord(bmesh,nodes[1])
-            c2 = Coord(bmesh,nodes[2])
-
-            # Add the displacements
-            pos = 3*(node1-1)+1; c1[1] +=  U[pos]*dscale
-            pos = 3*(node1-1)+2; c1[2] +=  U[pos]*dscale
-            pos = 3*(node2-1)+1; c2[1] +=  U[pos]*dscale
-            pos = 3*(node2-1)+2; c2[2] +=  U[pos]*dscale
-            pos = 3*(node1-1)+3; c1[3] +=  U[pos]*dscale
-            pos = 3*(node2-1)+3; c2[3] +=  U[pos]*dscale
-
-            # Tolerância para comparação
-            tol = 1.0/1E6 
-
-            # Adds a line to the plot
-            if x[ele] >cutout + tol
-                plot3d!([c1[1],c2[1]],[c1[2],c2[2]],[c1[3],c2[3]],
-                        color=:red,linewidth=(0.1+5*x[ele]),legend=false)
-            end
-        
-        end #ele
-
-    end #if length
-
-    # Display the plot
-    display(plot3d!())
-
-    # If name is not empty, saves the image
-    if !isempty(name)
-        savefig(name*".png")
-    end
+   # Coordenadas
+   x = bmesh.coord[node,1]
+   y = bmesh.coord[node,2]
+  
+   # Retorna as coordenadas
+   return x,y
 
 end
 
+function Coord(bmesh::Bmesh3D,node::Int64)
+
+  # Consistência do nó
+  0 < node <= bmesh.nn || throw("Coord::nó $node não é válido")
+
+  # Coordenadas
+  x = bmesh.coord[node,1]
+  y = bmesh.coord[node,2]
+  z = bmesh.coord[node,3]
+       
+  # Retorna as coordenadas
+  return x,y,z
+
+end
