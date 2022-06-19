c17bfe8faf59e117b427d166fc94f80dd64d4d77
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-19 19:45:29 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,14 @@
+using Test
+using BMesh
+
+# Method 2D
+include("test_b2D.jl")
+
+# Method 3D
+include("test_b3D.jl")
+
+# Base (truss 2D)
+include("test_base_truss2D.jl")
+
+# Base (truss 3D)
+include("test_base_truss3D.jl")
