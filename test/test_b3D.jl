406d364387f37c70458314094c10f8615044952f
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-19 19:47:07 -0400

    add tests for Bmesh3D

---
+++ test/test_b3D.jl
@@ -0,0 +1,46 @@
+@testset "Bmesh3D" begin
+
+    #
+    #    Valid inputs (no error)
+    #
+    nn = 2
+    ne = 1
+    coord = [0.0 0.0 0.0;
+             1.0 0.0 0.0]
+    connect = [1 2]
+    Lx = 1.0
+    Ly = 1.0
+    Lz = 1.0
+    nx = 1
+    ny = 1
+    nz = 1
+    etype = :truss3D
+
+    # truss3D
+    @test isa(Bmesh3D(etype,nn,ne,coord,connect,Lx,Ly,Lz,nx,ny,nz),Bmesh3D)
+
+    #
+    # Should throw assertion error 
+    # @assert size(coord,1)==nn "Bmesh3D:: number of rows in coord must be equal to the number of nodes
+    # 
+    @test_throws AssertionError Bmesh3D(etype,10,ne,coord,connect,Lx,Ly,Lz,nx,ny,nz)
+
+    #
+    # Should throw assertion error 
+    # @assert size(coord,2)==3 "Bmesh3D:: number of columns in coord must be equal to 3"
+    #
+    wcoord = [0.0 0.0 ; 1.0 0.0]
+    @test_throws AssertionError Bmesh3D(etype,nn,ne,wcoord,connect,Lx,Ly,Lz,nx,ny,nz)
+
+    #
+    # Should throw assertion error 
+    # @assert size(connect,1)==ne "Bmesh3D:: number of rows in connect must be equal to the number of elements"
+    wconnect = [1 2 ; 3 4]
+    @test_throws AssertionError Bmesh3D(etype,nn,ne,coord,wconnect,Lx,Ly,Lz,nx,ny,nz)
+
+    #
+    # Should throw assertion error 
+    # @assert etype==:truss3D "Bmesh3D:: just :truss3D by now"
+    @test_throws AssertionError Bmesh3D(:other,nn,ne,coord,connect,Lx,Ly,Lz,nx,ny,nz)
+
+end # Bmesh3D
