3897b07a3f0b7b34379afe021fe671cdc9966218
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-20 17:33:32 -0400

    overloading plot for bmesh2D and 3D
    Just truss2D and 3D by now

--- src/show.jl
+++ src/show.jl
@@ -2,14 +2,8 @@
 #
 # 2D
 #
-function Plot_structure(bmesh::Bmesh2D;
-                        U=[],
-                        dscale = 1.0,
-                        x=[],
-                        N = [],
-                        cutout = 1E-3,
-                        name=""
-                        )
+import Plots:plot
+function plot(bmesh::Bmesh2D;  name="" )
    
   
     # Local aliases
@@ -17,23 +11,11 @@ function Plot_structure(bmesh::Bmesh2D;
  
     # Create the plot
     plot();
-  
-    # Testa por x. Se o usuário não informou, usamos unitário
-    if length(x)==0
-        x = ones(ne)
-    end
-
+ 
     println("Generating the visualization...please wait")
-
-    # Se o vetor N for vazio, enchemos com 1.0
-    if isempty(N)
-         N = ones(ne)
-    end
-    
-
+  
     # Loop over the elements, extracting the nodes
-    # and their coordinates
-    
+    # and their coordinates  
     for ele=1:ne
 
         # recover the nodes
@@ -43,72 +25,12 @@ function Plot_structure(bmesh::Bmesh2D;
         c1 = Coord(bmesh,nodes[1])
         c2 = Coord(bmesh,nodes[2])
 
-        # Tolerância para comparação
-        tol = 1.0/1E6 
-
-        # Se N[ele] <0.0 -> azul
-        # Se N[ele] >0.0 -> vermelho
-        # Se N[ele] = 0.0 -> verde
-        cor = :blue
-        if N[ele]>0.0 
-            cor = :red
-        end    
-
-        # Se for numericamente próximo de zero
-        if isapprox(abs(N[ele]),0.0,atol=1E-3)
-            cor = :green
-        end
-
         # Adds a line to the plot
         # só mostra a linha se o x[ele] > do que o valor de corte
-        if x[ele]>cutout+tol 
-               plot!([c1[1],c2[1]],[c1[2],c2[2]],
-                      color=cor,linewidth=(0.1+5*x[ele]),legend=false)
-         end
-
+        plot!([c1[1],c2[1]],[c1[2],c2[2]],legend=false)
+        
     end #ele
-
-    # If the displacement vector is given
-    if length(U)>0
-
-        # Check size
-        @assert length(U)==2*bmesh.nn "Plot_structure:: length of U must be 2*number of nodes"
-    
-        # Normalize
-        U ./= norm(U)
-
-        # Loop over the elements. Now, the coordinates of each
-        # node are given by the initial coordinates
-        # plus the displacement*dscale
-        for ele=1:ne
-
-            # recover the nodes
-            nodes = Conect(bmesh,ele)
-            
-            # recover the coordinates
-            c1 = Coord(bmesh,nodes[1])
-            c2 = Coord(bmesh,nodes[2])
-
-            # Add the displacements
-            pos = 2*(nodes[1]-1)+1; c1[1] +=  U[pos]*dscale
-            pos = 2*(nodes[1]-1)+2; c1[2] +=  U[pos]*dscale
-            pos = 2*(nodes[2]-1)+1; c2[1] +=  U[pos]*dscale
-            pos = 2*(nodes[2]-1)+2; c2[2] +=  U[pos]*dscale
-
-            
-            # Tolerância para comparação
-            tol = 1.0/1E6 
-
-            # Adds a line to the plot
-            if x[ele] >cutout + tol
-                plot!([c1[1],c2[1]],[c1[2],c2[2]],
-                    color=:red,linewidth=(0.1+5*x[ele]),legend=false)
-            end
-
-        end #ele
-
-    end #if length
-
+  
     # Display the plot
     display(plot!())
 
@@ -124,14 +46,8 @@ end
 #
 # 3D
 #
-function Plot_structure(bmesh::Bmesh3D;
-                        U=[],
-                        dscale = 1.0,
-                        x=[],
-                        N = [],
-                        cutout = 1E-3,
-                        name=""
-                        )
+import Plots:plot
+function plot(bmesh::Bmesh3D; name="")
 
 
     # Local aliases
@@ -140,22 +56,11 @@ function Plot_structure(bmesh::Bmesh3D;
     # Starts the plot
     plot3d();
 
-    # Testa por x. Se o usuário não informou, usamos unitário
-    if length(x)==0
-        x = ones(ne)
-    end
-
     println("Generating the visualization...please wait")
-
-    # Se o vetor N for vazio, enchemos com 1.0
-    if isempty(N)
-        N = ones(ne)
-    end
 
 
     # Loop over the elements, extracting the nodes
     # and their coordinates
-
     for ele=1:ne
 
         # recover the nodes
@@ -165,73 +70,11 @@ function Plot_structure(bmesh::Bmesh3D;
         c1 = Coord(bmesh,nodes[1])
         c2 = Coord(bmesh,nodes[2])
 
-        # Tolerância para comparação
-        tol = 1.0/1E6 
-
-        # Se N[ele] <0.0 -> azul
-        # Se N[ele] >0.0 -> vermelho
-        # Se N[ele] = 0.0 -> verde
-        cor = :blue
-        if N[ele]>0.0 
-            cor = :red
-        end    
-
-        # Se for numericamente próximo de zero
-        if isapprox(abs(N[ele]),0.0,atol=1E-3)
-            cor = :green
-        end
-
         # Adds a line to the plot
         # só mostra a linha se o x[ele] > do que o valor de corte
-        if x[ele]>cutout+tol 
-            plot3d!([c1[1],c2[1]],[c1[2],c2[2]],[c1[3],c2[3]],
-                    color=cor,linewidth=(0.1+5*x[ele]),legend=false)
-        end
+        plot3d!([c1[1],c2[1]],[c1[2],c2[2]],[c1[3],c2[3]],legend=false)
 
     end #ele
-
-    # If the displacement vector is given
-    if length(U)>0
-
-        # Check size
-        @assert length(U)==3*bmesh.nn "Plot_structure:: length of U must be 3*number of nodes"
-    
-        # Normalize
-        U ./= norm(U)
-
-        # Loop over the elements. Now, the coordinates of each
-        # node are given by the initial coordinates
-        # plus the displacement*dscale
-        for ele=1:ne
-
-            # recover the nodes
-            nodes = Conect(bmesh,ele)
-                    
-            # recover the coordinates
-            c1 = Coord(bmesh,nodes[1])
-            c2 = Coord(bmesh,nodes[2])
-  
-            # Add the displacements
-            pos = 3*(nodes[1]-1)+1; c1[1] +=  U[pos]*dscale
-            pos = 3*(nodes[1]-1)+2; c1[2] +=  U[pos]*dscale
-            pos = 3*(nodes[1]-1)+3; c1[3] +=  U[pos]*dscale
-  
-            pos = 3*(nodes[2]-1)+1; c2[1] +=  U[pos]*dscale
-            pos = 3*(nodes[2]-1)+2; c2[2] +=  U[pos]*dscale
-            pos = 3*(nodes[2]-1)+3; c2[3] +=  U[pos]*dscale
-
-            # Tolerância para comparação
-            tol = 1.0/1E6 
-
-            # Adds a line to the plot
-            if x[ele] >cutout + tol
-                plot3d!([c1[1],c2[1]],[c1[2],c2[2]],[c1[3],c2[3]],
-                        color=:red,linewidth=(0.1+5*x[ele]),legend=false)
-            end
-        
-        end #ele
-
-    end #if length
 
     # Display the plot
     display(plot3d!())
