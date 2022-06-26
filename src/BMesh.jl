d7e4d1231aa543c0e995fe591dd44372e877f553
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-26 08:04:22 -0400

    Export Bmesh_solid3D

--- src/BMesh.jl
+++ src/BMesh.jl
@@ -9,6 +9,7 @@ module BMesh
    include("bmesh_truss_2d.jl")
    include("bmesh_truss_3d.jl")
    include("bmesh_solid_2d.jl")
+   include("bmesh_solid_3d.jl")
    include("base.jl")
    include("rotation.jl")
    include("show.jl")
@@ -16,7 +17,7 @@ module BMesh
 
    export Bmesh, Bmesh2D, Bmesh3D
    export Bmesh_truss_2D, Bmesh_truss_3D
-   export Bmesh_solid_2D
+   export Bmesh_solid_2D, Bmesh_solid_3D
    export Rotation, Rotation2D, Rotation3D, Rotations, T_matrix
    export Coord, Conect, DOFs, Length
    export Plot_structure
