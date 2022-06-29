9696a044a077a24f8851b97bc3756db81ba2a26c
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-29 13:16:16 -0400

    Update rotation.jl

--- src/rotation.jl
+++ src/rotation.jl
@@ -173,7 +173,7 @@ struct Rotation2D <: Rotation
 """
 Rotation
 
-    Rotation(bmesh::Bmesh, ele::Int64, α=0.0)
+    Rotations(bmesh::Bmesh, ele::Int64, α=0.0)
 
 General driver for Rotation. It deploys the correct method for either 2D or 3D problems.
 """
