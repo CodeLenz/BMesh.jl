93f2141869cfc608ebb0caa1172dd9e7660ff1f7
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-29 09:42:01 -0300

    Add docstrings

--- src/types.jl
+++ src/types.jl
@@ -1,9 +1,30 @@
 # Supertype
+"""
+Supertype for BMesh2D and BMesh3D 
+"""
 abstract type Bmesh end
 
 #
 # basic structure 2D
 #
+"""
+Basic structure for 2D meshes
+
+    Bmesh2D(etype::Symbol,nn::Int64,ne::Int64,coord::Matrix{Float64},connect::Matrix{Int64},
+            Lx::Float64, Ly::Float64, nx::Int64, ny::Int64)  
+
+where:
+
+    etype   = :truss2D or :solid2D
+    nn      = number of nodes
+    ne      = number of elements
+    coord   = nn x 2 matrix with nodal coordinates (x y)
+    connect = ne x {2,4} matrix with element connectivities. 2 for :truss2D and 4 for :solid2D
+    Lx      = horizontal (X) length (if using a rectangular domain)
+    nx      = number of divisions (elements) in the horizontal direction 
+    Ly      = vectival (Y) length (if using a rectangular domain)
+    ny      = number of divisions (elements) in the vertical direction 
+"""
 struct Bmesh2D <: Bmesh
  
     # Element type
@@ -50,6 +71,26 @@ end
 #
 # basic structure 3D
 #
+"""
+Basic structure for 3D meshes
+
+    Bmesh3D(etype::Symbol,nn::Int64,ne::Int64,coord::Matrix{Float64},connect::Matrix{Int64},
+            Lx::Float64, Ly::Float64, Lz::Float64,nx::Int64, ny::Int64, nz::Int64)
+    
+where:
+
+    etype   = :truss2D or :solid2D
+    nn      = number of nodes
+    ne      = number of elements
+    coord   = nn x 3 matrix with nodal coordinates (x y z)
+    connect = ne x {2,8} matrix with element connectivities. 2 for :truss3D and 8 for :solid3D
+    Lx      = horizontal (X) length (if using a cubic domain)
+    nx      = number of divisions (elements) in the horizontal X direction 
+    Ly      = horizontal (Y) length (if using a cubic domain)
+    ny      = number of divisions (elements) in the horizontal Y direction 
+    Lz      = vertical (Z) length (if using a cubic domain)
+    nz      = number of divisions (elements) in the vertical direction 
+"""
 struct Bmesh3D <: Bmesh
  
     # Element type
