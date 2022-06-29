97fa1542d216b10b5a2636b57a5f0ad3feb6b7e7
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-29 10:24:05 -0300

    Add docstrings

--- src/bmesh_solid_3d.jl
+++ src/bmesh_solid_3d.jl
@@ -13,8 +13,37 @@
 #  advancing plane by plane. 
 #
 #
-function Bmesh_solid_3D(Lx::Float64,nx::Int64,Ly::Float64,ny::Int64,Lz::Float64,nz::Int64,
-    showtruss=false)
+"""
+Generate a 3D background mesh for 8-node solid elements (:solid3D)
+
+    Bmesh_solid_3D(Lx::Float64,nx::Int64,Ly::Float64,ny::Int64,Lz::Float64,nz::Int64)
+
+where
+
+    Lx  = length in X (horizonal direction)
+    nx  = number of divisions (elements) in X
+    Ly  = length in Y (horizonal direction)
+    ny  = number of divisions (elements) in Y
+    Lz  = lenght in Z (vertical direction)
+    nz  = number of divisions (elements) in Z
+
+returns
+
+    Bmesh3D(:solid3D,nn,ne,coord,connect,Lx,Ly,Lz,nx,ny,nz)
+
+where
+
+    nn      = number of nodes 
+    ne      = number of elements
+    coord   = matrix nn x 3 with nodal coordinates (x y z) 
+    connect = matrix ne x 8 with connectivities.
+
+
+Nodes are generated plane by plane, from z=0 to z=Lz.    
+Connectivities follow the same pattern.
+
+"""
+function Bmesh_solid_3D(Lx::Float64,nx::Int64,Ly::Float64,ny::Int64,Lz::Float64,nz::Int64)
 
     # Assertions
     @assert Lx>0 "Bmesh_solid_3D:: Lx must be > 0"
