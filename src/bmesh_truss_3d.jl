67ddbd20a3794142c392666bf3eb8635018ae395
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-29 10:25:24 -0300

    Add docstrings

--- src/bmesh_truss_3d.jl
+++ src/bmesh_truss_3d.jl
@@ -7,7 +7,6 @@
 # nx -> number of cells in x direction
 # ny -> number of cells in y direction
 # nz -> number of cells in z direction
-# show -> false/true
 #
 #  We first create the mesh in the z=0 (xy) plane
 #  advancing plane by plane. THEN, the connections
@@ -21,6 +20,36 @@
 #
 #
 #
+"""
+Generate a 3D background mesh for truss:3D elements
+
+    Bmesh_truss_3D(Lx::Float64,nx::Int64,Ly::Float64,ny::Int64,Lz::Float64,nz::Int64)
+
+where
+
+    Lx  = length in X (horizonal direction)
+    nx  = number of divisions (elements) in X
+    Ly  = length in Y (horizonal direction)
+    ny  = number of divisions (elements) in Y
+    Lz  = lenght in Z (vertical direction)
+    nz  = number of divisions (elements) in Z
+
+returns
+
+    Bmesh3D(:truss3D,nn,ne,coord,connect,Lx,Ly,Lz,nx,ny,nz)
+
+where
+
+    nn      = number of nodes 
+    ne      = number of elements
+    coord   = matrix nn x 3 with nodal coordinates (x y z) 
+    connect = matrix ne x 2 with connectivities.
+
+
+Nodes are generated plane by plane, from z=0 to z=Lz.    
+Connectivities follow the same pattern.
+
+"""
 function Bmesh_truss_3D(Lx::Float64,nx::Int64,Ly::Float64,ny::Int64,Lz::Float64,nz::Int64,
                         showtruss=false)
 
