3823795c04f443728f30e3c26d1257934838c22c
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-30 14:14:31 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using BMesh
 using Documenter
 
 makedocs(
-         sitename = "BMesh.jl",
+         sitename = "BMesh",
          modules  = [BMesh],
          pages=[
                 "Home" => "index.md"
